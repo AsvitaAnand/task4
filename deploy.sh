cb563522819ed322d770d5e8f2f14ba290915f58
AsvitaAnand <127740114+AsvitaAnand@users.noreply.github.com>
2025-02-06 11:50:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u asvita12 -p asvinu@12
-    docker tag test asvita12/task3
-    docker push asvita12/task3
+    docker tag test asvita12/final
+    docker push asvita12/final
     
