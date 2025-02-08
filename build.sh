9f76bcb8b00ac432ba1ce7fb3c658f74176f6391
AsvitaAnand <127740114+AsvitaAnand@users.noreply.github.com>
2025-02-08 10:23:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker build -t test .
+docker build -t task4 .
+echo hey
+docker login -u asvita12 -p asvinu@12
+docker tag task4 asvita12/task4
+docker push asvita12/task4
+kubectl apply -f deploy.yaml
+kubectl apply -f svc.yaml
